645814933540bc5d42ef242bc849f6da356ddd5d
Katrina <blueyed_qt@hotmail.com>
2015-12-20 10:21:14 -0800

    Added tests for Post to RSpec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires a title' do 
+  	post = Post.new(title: '')
+  	post.valid?
+  	expect(post.errors[:title].any?).to eq(true)
+  end
+
+  it 'requires content' do 
+  	post = Post.new(content: '')
+  	post.valid?
+  	expect(post.errors[:content].any?).to eq(true)
+  end
+
+  describe 'relationships' do 
+  	before do 
+  		@user = User.create(first_name: 'Kobe', last_name: 'Bryant', email_address: 'kobe@lakers.com')
+  		@blog = Blog.ceate(name: 'A Blog', description: 'This is a blog')
+  		@post = Post.create(blog: @blog, user: @user, title: 'Post Title', content: 'Post content')
+  	end
+
+  	it 'belongs to a user' do 
+  		post = @user.posts.create(blog: @blog, title: 'Post title', content: 'Post content')
+  		expect(post.user).to eq(@user)
+  	end
+
+  	it 'belongs to a blog' do 
+  		post = @blog.posts.create(user: @user, title: 'Post title', content: 'Post content')
+  		expect(post.blog).to eq(@blog)
+  	end
+
+  	it ' has many messages' do 
+  		message1 = @post.messages.create(user: @user, author: 'Phil Jackson', message: 'Zenmaster')
+  		message2 = @post.messages.create(user: @user, author: 'Andrew Bynum', message: 'Zen grasshopper')
+  		expect(@post.messages).to include(message1)
+  		expect(@post.messages).to include(message2)
+  	end
+  end
 end
