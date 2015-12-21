b414b035712f3271ccc3e246bc2d7e70d99fafeb
Katrina <blueyed_qt@hotmail.com>
2015-12-20 17:28:16 -0800

    Added tests to Owner model

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Owner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do 
+  	before do 
+  		@user = User.create(first_name: 'Kobe', last_name: 'Bryant', email_address: 'kobe@lakers.com')
+  		@blog = Blog.create(name: 'A Blog', description: 'This is a blog')
+  	end
+
+  	it 'belongs to a user' do 
+  		owner = @user.owners.create(blog: @blog)
+  		expect(owner.user).to eq(@user)
+  	end
+
+  	it 'belongs to a blog' do 
+  		owner = @blog.owners.create(user: @user)
+  		expect(owner.blog).to eq(@blog)
+  	end
+  end
 end
